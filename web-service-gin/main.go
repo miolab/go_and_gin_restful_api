59b1a1868e9b9e0bdf216cfb05b77670292969e8
im <poson_hemp@yahoo.co.jp>
2022-04-21 12:41:53 +0900

    feat/ getAlbums (with Gin)

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+
+	router.Run("localhost:8080")
+}
+
 // album represents data about a record album.
 type album struct {
 	ID     string  `json:"id"`
@@ -10,7 +23,12 @@ type album struct {
 
 // albums slice to seed record album data.
 var albums = []album{
-	{ID: "1", Title: "Blue Train",　Artist: "John Coltrane", Price: 56.99},
-  {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-  {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+// getAlbums responds with the list of all albums as JSON.
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
 }
